f45b5160c5967b8b88ff18296902d14ce7835b6f
Rushimanikanata Lingam <158359799+rushimani2@users.noreply.github.com>
2024-09-02 00:14:08 +0530

    Update Record.jsx

--- mern/frontend/src/components/Record.jsx
+++ mern/frontend/src/components/Record.jsx
@@ -60,7 +60,7 @@ export default function Record() {
         });
       } else {
         // if we are updating a record we will PATCH to /record/:id.
-        response = await fetch(`http://localhost:5050/record/${params.id}`, {
+        response = await fetch(`http://18.207.220.197:5050/record/${params.id}`, {
           method: "PATCH",
           headers: {
             "Content-Type": "application/json",

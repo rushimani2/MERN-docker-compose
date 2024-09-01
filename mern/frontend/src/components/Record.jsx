a934c1680c74fc7dabee6a673599b8960ac09346
Rushimanikanata Lingam <158359799+rushimani2@users.noreply.github.com>
2024-09-02 00:13:30 +0530

    Update Record.jsx

--- mern/frontend/src/components/Record.jsx
+++ mern/frontend/src/components/Record.jsx
@@ -17,7 +17,7 @@ export default function Record() {
       if(!id) return;
       setIsNew(false);
       const response = await fetch(
-        `http://localhost:5050/record/${params.id.toString()}`
+        `http://18.207.220.197:5050/record/${params.id.toString()}`
       );
       if (!response.ok) {
         const message = `An error has occurred: ${response.statusText}`;
@@ -51,7 +51,7 @@ export default function Record() {
       let response;
       if (isNew) {
         // if we are adding a new record we will POST to /record.
-        response = await fetch("http://localhost:5050/record", {
+        response = await fetch("http://18.207.220.197:5050/record", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
